b049173c307b097b29f188112228f3677c4c77ab
zohrehchenari <zohrehchenari2874@gmail.com>
2025-04-26 23:26:39 +0330

    create services

--- js/main.js
+++ js/main.js
@@ -1,7 +1,37 @@
 
-const navToggler= document.querySelector(".nav__toggler");
-const navbar= document.querySelector(".nav");
-navToggler.addEventListener("click",(e)=>{
-  console.log("clicked");
-  navbar.classList.toggle("nav__expanded");
-})
+// navbar
+// const navToggler= document.querySelector(".nav__toggler");
+// const navbar= document.querySelector(".nav");
+// navToggler.addEventListener("click",(e)=>{
+//   console.log("clicked");
+//   navbar.classList.toggle("nav__expanded");
+// });
+
+
+// services
+
+
+const tabs = document.querySelectorAll(".tab");
+const tabContents = document.querySelectorAll(".tab-content");
+tabs.forEach((tab) => {
+  tab.addEventListener("click", (e) => {
+    const targeTabContent = document.querySelector(`#${tab.dataset.tabTarget}`);
+
+    // REMOVE PREVIOUS ACTIVED CLASSES
+    tabs.forEach((tab) => tab.classList.remove("active"));
+    tabContents.forEach((tabContent) => tabContent.classList.remove("active"));
+
+    // ADD NEW ACTIVE CLASSES
+    tab.classList.add("active");
+    targeTabContent.classList.add("active");
+  });
+});
+
+// const accordionHeaders = document.querySelectorAll(".accordion__header");
+// const accordion = document.querySelector(".accordion");
+// accordionHeaders.forEach((item) => {
+//   item.addEventListener("click", (e) => {
+//     console.log(e.target.parentElement);
+//     e.target.parentElement.classList.toggle("accordion__expanded");
+//   });
+// });
