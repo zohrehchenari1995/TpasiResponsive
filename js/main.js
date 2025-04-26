a5369a44e4207d098e9c7e6358709ce53254c8d8
zohrehchenari <zohrehchenari2874@gmail.com>
2025-04-27 02:01:27 +0330

    add all components in index-html

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 
 // navbar
-// const navToggler= document.querySelector(".nav__toggler");
-// const navbar= document.querySelector(".nav");
-// navToggler.addEventListener("click",(e)=>{
-//   console.log("clicked");
-//   navbar.classList.toggle("nav__expanded");
-// });
+const navToggler= document.querySelector(".nav__toggler");
+const navbar= document.querySelector(".nav");
+navToggler.addEventListener("click",(e)=>{
+  console.log("clicked");
+  navbar.classList.toggle("nav__expanded");
+});
 
 
 // services
